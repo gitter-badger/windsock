3da3c752effd5e0b61fe28d8b9d3e5e60572a20a
Ben Sawyer <bsawyer@users.noreply.github.com>
2014-11-03 16:04:09 -0500

    removed debug dir and coverage tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,10 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    mocha = require('gulp-mocha'),
-    coverage = require('gulp-coverage');
+    concat = require('gulp-concat');
 
 gulp.task('concat', function(){
     gulp.src('./src/*.js')
         .pipe(concat('windsock.js'))
         .pipe(gulp.dest('./build/'));
-});
-
-gulp.task('test', function () {
-    return gulp.src(['./test/*.js'], { read: false })
-        .pipe(coverage.instrument({
-            pattern: ['./src/*'],
-            debugDirectory: '.coverdata'
-        }))
-        .pipe(mocha())
-        .pipe(coverage.report({
-            outFile: '.coverdata/coverage.html'
-        }));
 });
 
 gulp.task('default', ['concat'], function(){
